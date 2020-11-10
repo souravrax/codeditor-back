14c580472f3fa230d8a0a010a484ce6210dbfa36
Sourav Rakshit <rakshitsourav3@gmail.com>
2020-11-10 13:01:29 +0530

    Added TTL (https://docs.mongodb.com/manual/tutorial/expire-data/)

--- Routers/share.js
+++ Routers/share.js
@@ -1,6 +1,8 @@
+"use strict"
+// Share Router
 const express = require("express");
 const router = express.Router();
-const assert = require("assert");
+const ms = require("ms");
 
 const mongoose = require("mongoose");
 const SharedCode = require("../models/sharedCodeModel");
@@ -20,13 +22,45 @@ db.once("open", () => {
     console.log("Connected to database");
 });
 
+const mapExpireIndexToValues = [
+    "1m",
+    "5m",
+    "30m",
+    "2hrs",
+    "12hrs",
+    "1 day",
+    "5 days",
+];
+for (let i of mapExpireIndexToValues) {
+    console.log(ms(i));
+}
+
+//! Share feature
 router.post("/export", (req, res) => {
-    const { code, input, language } = req.body;
+    const { code, input, language, expireIndex } = req.body;
+
+    // If the provided index is not valid, then respond with 404(Not Found) message
+    if (expireIndex < 0 || expireIndex >= mapExpireIndexToValues.length) {
+        res.status(404).json({
+            success: false,
+        });
+    }
+
+    //? Getting the creation date and the expiration date
+    const currentDate = new Date();
+    const expireDate = new Date(
+        currentDate.getTime() + ms(mapExpireIndexToValues[expireIndex])
+    );
+    //? Data to be saved into the database
     const entry = new SharedCode({
         code: code,
         input: input,
         language: language,
+        date: currentDate,
+        expire_at: expireDate,
     });
+
+    //? Inserting the data to database
     entry.save((err, doc) => {
         if (err) {
             return console.error(err);
@@ -34,11 +68,12 @@ router.post("/export", (req, res) => {
         console.log(doc);
         res.status(200).json({
             success: true,
-            id: doc["_id"]
+            id: doc["_id"],
         });
     });
 });
 
+// Import shared code feature
 router.post("/import", (req, res) => {
     const { id } = req.body;
     SharedCode.find(
@@ -49,18 +84,18 @@ router.post("/import", (req, res) => {
             if (err) {
                 console.error(err);
                 res.status(500).json({
-                    success: false
+                    success: false,
                 });
             }
             if (doc.length == 0) {
                 res.status(200).json({
                     success: false,
-                })
+                });
             } else {
                 res.status(200).json({
                     success: true,
-                    data: doc[0]
-                })
+                    data: doc[0],
+                });
             }
         }
     );
--- models/sharedCodeModel.js
+++ models/sharedCodeModel.js
@@ -1,4 +1,4 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const SharedCodeSchema = new Schema({
@@ -17,7 +17,13 @@ const SharedCodeSchema = new Schema({
     date: {
         type: Date,
         default: Date.now,
-    }
+    },
+    expire_at: {
+        type: Date,
+        default: Date.now,
+    },
 });
+
+SharedCodeSchema.index({ expire_at: 1 }, { expires: 0 });
 
 module.exports = mongoose.model("SharedCodes", SharedCodeSchema);
